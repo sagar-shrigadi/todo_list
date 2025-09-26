c29db62473fccbdbcbac486b2e673c952a62d53b
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-09-26 21:24:05 +0530

    feat(todo):add modal for todo input & rendering it
    - Created modal with styling for adding new todos
    - Implemented open/close functionality with buttons
    - Updated todo container from div to ul for semantic structure in html
    - Added submit handler to process form input and create task object
    - Rendered task elements (li, checkbox to mark it as complete, p, delete task button) dynamically
    - Styled the dynamically created task container
    - Reset form inputs and close modal after submission

--- src/index.js
+++ src/index.js
@@ -1,2 +1,67 @@
 import './reset.css';
 import './styles.css';
+import deleteImageSvg from "./assets/icons/close_black.svg";
+
+const addTaskButton = document.querySelector(".addTasksContainer");
+const addTaskModal = document.querySelector("#addTasksDialog");
+const addTaskForm = document.querySelector(".addTasksForm");
+const cancelModalButton = document.querySelector(".cancelModal");
+const toDoLists = document.querySelector(".toDoLists");
+
+const tasksArray = [];
+
+addTaskButton.addEventListener("click", ()=>{
+    addTaskModal.showModal();
+});
+
+cancelModalButton.addEventListener("click", ()=>{
+    addTaskModal.close();
+});
+
+addTaskForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    const taskTitle = document.querySelector("#taskTitle").value;
+    const taskDueDate = document.querySelector("#taskDueDate").value;
+
+    const newTask = CreateTasks(taskTitle, taskDueDate);
+    
+    tasksArray.push(newTask);
+    taskAdditionDOM(newTask);
+
+    addTaskForm.reset();
+    addTaskModal.close();
+});
+
+function CreateTasks(title, dueDate, isCompleted = false){
+
+    const finalDueDate = dueDate || "No due date";
+
+    return {
+        title,
+        finalDueDate,
+        isCompleted
+    };
+}
+
+function taskAdditionDOM(taskToAdd){
+    const toDoContainer = document.createElement("li");
+    toDoContainer.classList.add("taskContainer");
+
+    const toDoCheckbox = document.createElement("input");
+    toDoCheckbox.type = "checkbox"
+
+    const toDoTitle = document.createElement("p");
+    toDoTitle.textContent = taskToAdd.title;
+
+    const toDoDueDate = document.createElement("p");
+    toDoDueDate.textContent = taskToAdd.finalDueDate;
+
+    const toDoDeleteButton = document.createElement("button");
+    const deleteImage = document.createElement("img");
+    deleteImage.src = deleteImageSvg;
+    toDoDeleteButton.appendChild(deleteImage);
+
+    toDoContainer.append(toDoCheckbox, toDoTitle, toDoDueDate, toDoDeleteButton);
+    toDoLists.appendChild(toDoContainer);
+}
