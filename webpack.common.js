cba8474691eed1d5b016e9c71a1747b689d0d468
frost3dWave <181492974+frost3dWave@users.noreply.github.com>
2025-10-04 20:12:34 +0530

    feat(fix): fixed the public path in webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-        publicPath: '/',
+        publicPath: './',
     },
     plugins: [
         new HtmlWebpackPlugin({
